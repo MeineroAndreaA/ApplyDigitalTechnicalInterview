079c0ae1e855526424f1dfcac14e2986005d1dcc
Andrea <meinero.andrea.a@gmail.com>
2024-06-04 12:35:26 -0300

    Add Room & entity
    HitsEntity

--- app/src/main/java/com/aam/applydigitaltechnicalinterview/MainActivity.kt
+++ app/src/main/java/com/aam/applydigitaltechnicalinterview/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import com.aam.applydigitaltechnicalinterview.data.core.RetrofitModule
+import com.aam.applydigitaltechnicalinterview.domain.remote.RemoteDataSource
 import com.aam.applydigitaltechnicalinterview.presentation.theme.ApplyDigitalTechnicalInterviewTheme
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        runBlocking { RemoteDataSource(RetrofitModule.api).getHackersNew() }
         enableEdgeToEdge()
         setContent {
             ApplyDigitalTechnicalInterviewTheme {
