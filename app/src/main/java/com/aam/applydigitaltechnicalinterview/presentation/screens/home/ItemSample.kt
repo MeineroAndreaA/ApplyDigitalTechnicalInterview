ebb84bdc7976a58c2bdf12b66bae4404063abdf7
Andrea <meinero.andrea.a@gmail.com>
2024-06-05 12:42:31 -0300

    Fix margins Item

--- app/src/main/java/com/aam/applydigitaltechnicalinterview/presentation/screens/home/ItemSample.kt
+++ app/src/main/java/com/aam/applydigitaltechnicalinterview/presentation/screens/home/ItemSample.kt
@@ -1,33 +1,52 @@
 package com.aam.applydigitaltechnicalinterview.presentation.screens.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.aam.applydigitaltechnicalinterview.data.model.response.Hits
+import com.aam.applydigitaltechnicalinterview.presentation.screens.home.utils.formatDate
+import com.aam.applydigitaltechnicalinterview.presentation.theme.Grey40
 
 @Composable
 fun ItemSample(hits: Hits) {
-    Row(
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable {  }
+            .clickable { }
     ) {
+        (if (hits.title.isNullOrEmpty()) hits.storyTitle else hits.title)?.let {
+            Text(
+                text = it,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(bottom = 8.dp, start = 8.dp, top = 8.dp)
+            )
+        }
+
         Text(
-            text = hits.storyTitle.toString(),
-            fontSize = 20.sp,
-            modifier = Modifier.align(Alignment.CenterVertically)
+            text = "${hits.author} - ${formatDate(hits.createdAt.toString())}",
+            fontSize = 17.sp,
+            color = Grey40,
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(bottom = 8.dp, start = 8.dp, top = 8.dp)
+        )
+
+        Divider(
+            color = Color.Black,
+            thickness = 1.dp,
+            modifier = Modifier.padding(top = 8.dp)
         )
     }
 
--- app/src/main/java/com/aam/applydigitaltechnicalinterview/presentation/theme/Color.kt
+++ app/src/main/java/com/aam/applydigitaltechnicalinterview/presentation/theme/Color.kt
@@ -10,4 +10,4 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Grey40 = Color(0x43959A9E)
+val Grey40 = Color(0xFF5A5A5A)
